73a544ec51ac7f3179346b32f57d70ad7ff73e96
JoniEmann <joni.emannuel1@gmail.com>
2024-12-05 08:06:15 -0300

    Criando um array para as perguntas e para as respostas;

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ function resultado() {
     localStorage.setItem("freddy", contFreddy);
     localStorage.setItem("bonnie", contBonnie);
     localStorage.setItem("chica", contChica);
-    localStorage.setItem("pidgeotto", contFoxy);
+    localStorage.setItem("foxy", contFoxy);
 
     let maior = 0
     let pagina;
@@ -45,10 +45,38 @@ function resultado() {
     }
 
     if(contFoxy > maior) {
-        pagina = "pidgeotto"
+        pagina = "foxy"
     }
 
     setTimeout(() => {
         window.open(pagina + ".html")
     }, 5000);
 }
+
+let perguntas = [
+                 "Qual a cor do seu animatrônico favorito?", // 1
+                 "Qual é a habilidade do seu animatrônico favorito?", // 2
+                 "Qual é o seu acessório favorito?", // 3
+                 "Qual é o seu lugar favorito no restaurante?", // 4
+                 "Qual é a sua expressão favorita?", // 5
+                 "Qual é o seu comportamento favorito?", // 6
+                 "Qual é o seu momento favorito?", // 7
+                 "Qual é a sua comida favorita?", // 8
+                 "Qual é o seu medo?", // 9
+                 "Qual é o seu tipo de música favorita?" // 10
+                ]
+
+let respostas = [
+                 "Freddy", "Bonnie", "Chica", "Foxy", // PERGUNTA Nº1
+                 "Cantar", "Tocar guitarra", "Distrair com comida", "Correr rápido", // PERGUNTA Nº2
+                 "Chápeu", "Guitarra", "Cupcake", "Pirata", // PERGUNTA Nº3
+                 "Palco", "Sala de jogos", "Cozinha", "Corredor", // PERGUNTA Nº4
+                 "Sorriso amigável", "Sorriso curioso", "Sorriso alegre", "Olhos brilhantes", // PERGUNTA Nº5
+                 "Brincar com crianças", "Fazer truques", "Cantar", "Espionar", // PERGUNTA Nº6
+                 "Durante o show", "Quando as luzes piscam", "Hora do lanche", "Quando está escondido", // PERGUNTA Nº7
+                 "Pizza", "Cenoura", "Bolo", "Peixe", // PERGUNTA Nº8
+                 "Ser desligado", "Ficar sozinho", "Não ser lembrada", "Perder a velocidade", // PERGUNTA Nº9
+                 "Canções infantis", "Rock", "Música alegre", "Música de pirata" // PERGUNTA Nº10
+
+]
+
