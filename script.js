c6c8d15801e8802820ae4f7ac15ac71c878fdd6b
MarcosFabSoftware2 <kauanplg2020@gmail.com>
2024-12-05 10:05:26 -0300

    Organizado o JavaScript e melhorado o design

--- script.js
+++ script.js
@@ -1,54 +1,67 @@
-function resultado() {
-    let contFreddy = 0; 
-    let contBonnie = 0;
-    let contChica = 0;
-    let contFoxy = 0;
+let contFreddy = 0; 
+let contBonnie = 0;
+let contChica = 0;
+let contFoxy = 0;
 
+function contagem() {
     let radios = document.querySelectorAll("input[type='radio']");
 
     radios.forEach(element => {
-        if(element.checked) {
-            if(element.value == "freddy") {
+        if (element.checked) {
+            if (element.value === "freddy") {
                 contFreddy++;
-            }else if(element.value == "bonnie") {
+            } else if (element.value === "bonnie") {
                 contBonnie++;
-            }else if(element.value == "chica") {
+            } else if (element.value === "chica") {
                 contChica++;
-            }else if(element.value == "foxy"){
+            } else if (element.value === "foxy") {
                 contFoxy++;
             }
         }
-    })
+    });
+}
 
+function resultado() {
     let resultado = `Freddy = ${contFreddy}<br> Bonnie = ${contBonnie}<br> Chica = ${contChica}<br> Foxy = ${contFoxy}`;
-
     document.querySelector("#resultado").innerHTML = resultado;
 
     localStorage.setItem("freddy", contFreddy);
     localStorage.setItem("bonnie", contBonnie);
     localStorage.setItem("chica", contChica);
-    localStorage.setItem("pidgeotto", contFoxy);
+    localStorage.setItem("foxy", contFoxy);
+}
 
-    let maior = 0
+function exibirResultado(pagina) {
+    let contagemVotos = localStorage.getItem(pagina);
+    document.querySelector("#votos").innerHTML = "<h1>Votos: " + contagemVotos + "</h1>";
+}
+
+function Maior() {
+    let maior = Math.max(contFreddy, contBonnie, contChica, contFoxy);
     let pagina;
 
-    if(contFreddy > maior) {
-        pagina = "freddy"
+    if (contFreddy === maior) {
+        pagina = "freddy";
+    } else if (contBonnie === maior) {
+        pagina = "bonnie";
+    } else if (contChica === maior) {
+        pagina = "chica";
+    } else if (contFoxy === maior) {
+        pagina = "foxy";
     }
 
-    if(contBonnie > maior) {
-        pagina = "bonnie"
-    }
+    return pagina;
+}
 
-    if(contChica > maior) {
-        pagina = "chica"
-    }
-
-    if(contFoxy > maior) {
-        pagina = "pidgeotto"
-    }
-
+function redirecionamento() {
+    let pagina = Maior();
     setTimeout(() => {
-        window.open(pagina + ".html")
+        window.open(pagina + ".html");
     }, 5000);
+}
+
+function resultadoFinal() {
+    contagem();
+    resultado();
+    redirecionamento();
 }
